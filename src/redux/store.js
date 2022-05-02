e698ca150bfe4838e558b50c72268ee7f09fbe92
rolandexplore93 <roland2rule@gmail.com>
2022-05-02 08:27:10 +0100

    Apply middleware in react-redux application

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
-import { createStore } from 'redux'
-// import cakeReducer from './cake/cakeReducer'
+import { applyMiddleware, createStore } from 'redux'
+import logger from 'redux-logger'
 import rootReducer from './rootReducer'
+// import cakeReducer from './cake/cakeReducer'
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(logger))
 
 export default store
